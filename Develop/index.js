678950b47da7b49d70f7cf63f902cd558400680a
Stephen Garcia <garciastephen5280@gmail.com>
2023-08-24 23:58:54 -0600

    added skeleton for questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,47 @@
 // TODO: Include packages needed for this application
-
+const inquire = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+];
 
-// TODO: Create a function to write README file
+// TODO: Create a function to write README file This might be like lines 50-56 in the mini project
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
