274a9d6d33a837c2107c8adac651fc6e862fdc83
Stephen Garcia <garciastephen5280@gmail.com>
2023-09-07 23:51:34 -0600

    made function for handling license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,23 @@
 // If there is no license, return an empty string
 const licenseChoices =['IBM Public License Version 1.0', 'MIT', 'ISC', 'Mozilla Public License 2.0'];
 
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === "GNU AGPLv3") {
+      return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+    } else if (license === "GNU GPLv3") {
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    } else if (license === "GNU LGPLv3") {
+      return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+    } else if (license === "Mozilla") {
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    } else if (license === "MIT") {
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (license === "Apache") {
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else {
+      return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    }
+  }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,8 +30,6 @@ function renderLicenseSection(license) {}
 
 
 // TODO: Create a function to generate markdown for README This will most likely be like copying a raw readme with # marks
-// const choiceIndex = licenseChoices.indexOf(license)
-// console.log(licenseColor[choiceIndex])
 function generateMarkdown({ title, description, installation, usage, license, guidelines, test, github, email }) {
     const licenseColor = ['IPL_1.0-blue', 'MIT-yellow', 'ISC-blue', 'MPL_2.0-brightgreen']; 
             const choiceIndex = licenseChoices.indexOf(license)
@@ -37,7 +51,8 @@ ${installation}
 # Usage
 ${usage}
 # License
-![](https://img.shields.io/badge/License-${licenseColor[licenseChoices]}.svg)
+${renderLicenseBadge(data.license)}
+This application is covered by the ${data.license} license. 
 # Contribution-Guidelines
 ${guidelines}
 # Test
